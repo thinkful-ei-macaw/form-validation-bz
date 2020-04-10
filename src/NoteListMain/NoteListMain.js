6cab390939368288d713ff9ed9ca57aff2775d41
Brannen Petit <bpetit940@gmail.com>
2020-04-10 17:18:22 -0500

    finished

--- src/NoteListMain/NoteListMain.js
+++ src/NoteListMain/NoteListMain.js
@@ -1,28 +1,29 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import Note from '../Note/Note'
-import CircleButton from '../CircleButton/CircleButton'
-import ApiContext from '../ApiContext'
-import { getNotesForFolder } from '../notes-helpers'
-import './NoteListMain.css'
+import React from "react";
+import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import Note from "../Note/Note";
+import CircleButton from "../CircleButton/CircleButton";
+import ApiContext from "../ApiContext";
+import { getNotesForFolder } from "../notes-helpers";
+import "./NoteListMain.css";
 
 export default class NoteListMain extends React.Component {
   static defaultProps = {
     match: {
       params: {}
     }
-  }
-  static contextType = ApiContext
+  };
+  static contextType = ApiContext;
 
   render() {
-    const { folderId } = this.props.match.params
-    const { notes = [] } = this.context
-    const notesForFolder = getNotesForFolder(notes, folderId)
+    const { folderId } = this.props.match.params;
+    const { notes = [] } = this.context;
+    console.log(folderId, notes);
+    const notesForFolder = getNotesForFolder(notes, folderId);
     return (
-      <section className='NoteListMain'>
+      <section className="NoteListMain">
         <ul>
-          {notesForFolder.map(note =>
+          {notesForFolder.map(note => (
             <li key={note.id}>
               <Note
                 id={note.id}
@@ -31,21 +32,21 @@ export default class NoteListMain extends React.Component {
                 history={this.props.history}
               />
             </li>
-          )}
+          ))}
         </ul>
-        <div className='NoteListMain__button-container'>
+        <div className="NoteListMain__button-container">
           <CircleButton
             tag={Link}
-            to='/add-note'
-            type='button'
-            className='NoteListMain__add-note-button'
+            to="/add-note"
+            type="button"
+            className="NoteListMain__add-note-button"
           >
-            <FontAwesomeIcon icon='plus' />
+            <FontAwesomeIcon icon="plus" />
             <br />
             Note
           </CircleButton>
         </div>
       </section>
-    )
+    );
   }
 }
