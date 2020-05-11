82881ff1a13e9f0c94b17e6a65d09c5e26d1785b
Connor McCormack <mccormack.connor.m@gmail.com>
2020-05-10 21:26:11 -0500

    deployed

--- src/config.js
+++ src/config.js
@@ -1,3 +1,25 @@
 export default {
-  API_ENDPOINT: "https://evening-sea-89091.herokuapp.com",
+  API_ENDPOINT: "https://mysterious-cove-41623.herokuapp.com",
+  API_KEY: process.env.REACT_APP_API_KEY,
 };
+
+// mine https://mysterious-cove-41623.herokuapp.com/
+// http://localhost:8000
+// heroku-postgresql:hobby-dev
+// Creating heroku-postgresql:hobby-dev on ⬢ mysterious-cove-41623... free
+// Database has been created and is available
+//  ! This database is empty. If upgrading, you can transfer
+//  ! data from another database with pg:copy
+// Created postgresql-aerodynamic-38795 as DATABASE_URL
+// Use heroku addons:docs heroku-postgresql to view documentation
+
+// === mysterious-cove-41623 Config Vars
+// DATABASE_URL: postgres://lxwbtdbfdfqiyt:1d68b9e08a6e07cecca4b315ff89edae9f8cd8d95678a18fa04ad140178954c1@ec2-35-171-31-33.compute-1.amazonaws.com:5432/d5crg3johnlnio
+
+// pg:credentials:URL
+// Connection information for default credential.
+// Connection info string:
+//    "dbname=d5crg3johnlnio host=ec2-35-171-31-33.compute-1.amazonaws.com port=5432 user=lxwbtdbfdfqiyt password=1d68b9e08a6e07cecca4b315ff89edae9f8cd8d95678a18fa04ad140178954c1 sslmode=require"
+// Connection URL:
+//    postgres://lxwbtdbfdfqiyt:1d68b9e08a6e07cecca4b315ff89edae9f8cd8d95678a18fa04ad140178954c1@ec2-35-171-31-33.compute-1.amazonaws.com:5432/d5crg3johnlnio
+
